d96c63063d0f2e138fb2cff19d214d631ab3bc92
rnabilal-ai <rifqinabilal25@gmail.com>
2025-01-22 11:00:59 +0700

    membuat form karyawan dan menambahkan link

---
+++ modul-karyawan/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
+</head>
+<body>
+<?php
+    include_once('../navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3>Data Karyawan</h3>
+            </div>
+            <div class="card-body">
+                <h4>otak atik</h4>
+                <p>coba aja untuk sesuka hatii</p>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-dark-subtle">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Data karyawan</a>
+          <a class="nav-link active" aria-current="page" href="/UAS_rifqinabilal/modul-karyawan/index.php">Data karyawan</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Ubah Password</a>
